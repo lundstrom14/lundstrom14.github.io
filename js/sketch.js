87450ab9f9ac9dac12509a17d0b26460fb6d6819
Mattias Lundström <mattias.lundstrom1214@gmail.com>
2020-10-23 00:02:03 +0200

    fourier n=1

--- js/sketch.js
+++ js/sketch.js
@@ -1,8 +1,45 @@
+//p5 introduction. Code from the coding train
+
+let time = 0;
+let wave = [];
+
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(800, 800);
+    frameRate(45);
 }
 
 function draw() {
-    background(220);
-    ellipse(50, 50, 80, 80);
+    background(0);
+    translate(250, 300);
+
+    let r = 100;
+    stroke(255);
+    noFill();
+    ellipse(0, 0, 2 * r);
+
+    let x = r * cos(time);
+    let y = r * sin(time);
+    let r2 = 50;
+    ellipse(x, y, 2 * r2);
+    line(0, 0, x, y);
+    stroke('rgba(255,204,0,0.5)');
+    line(x, y, 200, y);
+
+    translate(200, 0);
+    stroke('yellow');
+
+    beginShape();
+    wave.unshift(y); //save y value for wave vis
+    for (let i = 0; i < wave.length; i++) {
+        vertex(i, wave[i]);
+    }
+    endShape();
+
+    point(0, y);
+
+    if (wave.length > 700) {
+        wave.pop();
+    }
+
+    time += 0.04;
 }
