0d7d41575bfb6e99359d16f731eda1133445211e
Mattias Lundström <mattias.lundstrom1214@gmail.com>
2020-10-25 21:09:53 +0100

    fourier

--- js/sketch.js
+++ js/sketch.js
@@ -6,28 +6,47 @@ let wave = [];
 function setup() {
     createCanvas(800, 800);
     frameRate(45);
+    slider = createSlider(1, 15, 5, 2);
+    slider.position(200, 500);
+
 }
 
 function draw() {
     background(0);
+    fill(255);
+    stroke(255);
+    text('n = ' + slider.value(), slider.x + slider.width + 10, slider.y);
     translate(250, 300);
+    noFill();
 
     let r = 100;
-    stroke(255);
-    noFill();
-    ellipse(0, 0, 2 * r);
+    let x = 0;
+    let y = 0;
 
-    let x = r * cos(time);
-    let y = r * sin(time);
-    let r2 = 50;
-    ellipse(x, y, 2 * r2);
-    line(0, 0, x, y);
-    stroke('rgba(255,204,0,0.5)');
+
+    for (let i = 0; i <= slider.value(); i++) {
+        let prev_x = x;
+        let prev_y = y;
+
+        n = 2 * i + 1;
+        r = 100 * (4 / (n * PI));
+        x += r * cos(n * time);
+        y += r * sin(n * time);
+        stroke(255, 100);
+        ellipse(prev_x, prev_y, 2 * r);
+        stroke(255);
+        line(prev_x, prev_y, x, y);
+        //r = r / 1.5; // or using logs for smoother downsizing
+    }
+
+
+
+
+    stroke('rgba(255,204,0,0.5)'); // yellow w transparancy
+
     line(x, y, 200, y);
-
     translate(200, 0);
     stroke('yellow');
-
     beginShape();
     wave.unshift(y); //save y value for wave vis
     for (let i = 0; i < wave.length; i++) {
